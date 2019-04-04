0d4fb841146313e3813ee881e42e0f97cdf83ddf
Thầy Hùng <mrhung189@gmail.com>
2019-04-04 07:59:38 +0700

    Them ghi chu trong IndetityModels.cs

--- Test1/Models/IdentityModels.cs
+++ Test1/Models/IdentityModels.cs
@@ -15,6 +15,7 @@ namespace Test1.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
+            //them ghi chu trong code
         }
     }
 
